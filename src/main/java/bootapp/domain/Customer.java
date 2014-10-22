2d6e0f62d4bcb5e4189db3d161e14810bff4b8da
gr-terasawa <takeharu.terasawa@gmo-research.jp>
2014-10-22 19:13:37 +0900

    バリデーション実装

--- src/main/java/bootapp/domain/Customer.java
+++ src/main/java/bootapp/domain/Customer.java
@@ -5,23 +5,84 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * @author usr160056
  *
  */
 @Entity
+@Table(name = "customer")
 public class Customer {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 
-    @Column(nullable = false)
-    private String name;
+	@Column(name = "name")
+	@NotEmpty
+	private String name;
+
+	@Column(name = "kana")
+	@NotEmpty
+	private String kana;
+
+	@Column(name = "zip", length = 8)
+	private String zip;
+
+	@Column(name = "address")
+	private String address;
+
+	@Column(name = "birthday", length = 8)
+	private String birthday;
+
+	@Column(name = "phone", length = 12)
+	private String phone;
 
 	public Long getId() {
 		return id;
+	}
+
+	public String getKana() {
+		return kana;
+	}
+
+	public void setKana(String kana) {
+		this.kana = kana;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
 
 	public void setId(Long id) {
@@ -35,5 +96,9 @@ public class Customer {
 	public void setName(String name) {
 		this.name = name;
 	}
-    
+
+    public boolean isNew() {
+        return (this.id == null);
+    }
+
 }
--- src/main/java/bootapp/web/CustomerController.java
+++ src/main/java/bootapp/web/CustomerController.java
@@ -10,10 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
 
 import bootapp.domain.Customer;
 import bootapp.service.CustomerService;
@@ -25,6 +28,11 @@ public class CustomerController {
 	@Autowired
 	private CustomerService customerService;
 
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
     @RequestMapping(value = "customer/{id}", method = RequestMethod.GET)
     public String selectUser(@PathVariable Long id, Model model) {
     	Customer customer = customerService.selectCustomer(id);
@@ -33,14 +41,16 @@ public class CustomerController {
         return "index";
     }
 
-    @RequestMapping(value = "/customer" ,method = RequestMethod.POST)
-    public String createUser(@RequestParam @Valid String name) {
-    	Customer customer = new Customer();
-    	customer.setName(name);
-    	Customer savedCustomer = customerService.save(customer);
-    	log.info(savedCustomer.getId() + ":" + savedCustomer.getName());
-
-        return "redirect:/customer";
+    @RequestMapping(value = "/customer/add" ,method = RequestMethod.POST)
+    public String createUser(@Valid Customer customer, BindingResult result,
+            SessionStatus status) {
+        if (result.hasErrors()) {
+            return "add";
+        } else {
+            this.customerService.save(customer);
+            status.setComplete();
+            return "redirect:/customer";
+        }
     }
 
     @RequestMapping(value = "/customer" ,method = RequestMethod.GET)
@@ -55,6 +65,8 @@ public class CustomerController {
     @RequestMapping(value = "customer/add", method = RequestMethod.GET)
     public String add(Model model) {
     	log.info("/customer/add");
+    	Customer customer = new Customer();
+    	model.addAttribute("customer", customer);
         return "add";
     }
 
